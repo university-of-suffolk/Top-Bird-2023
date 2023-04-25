c59a558c4045ef3b52cd24461bbfeb2345ef9edc
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-04-25 09:54:54 +0100

    2504/2023
    initial 7 Scripts added

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This script will define the player's attributes, such as their score, cards they hold, and their turn order.
+
+
+public class Player : MonoBehaviour
+{
+    // Player attributes
+    public int playerID;
+    public int score;
+    public List<Card> hand = new List<Card>();
+
+    // Add a card to the player's hand
+    public void AddCard(Card card)
+    {
+        hand.Add(card);
+    }
+
+    // Remove a card from the player's hand
+    public void RemoveCard(Card card)
+    {
+        hand.Remove(card);
+    }
+
+    // Get the player's score
+    public int GetScore()
+    {
+        return score;
+    }
+
+    // Increase the player's score
+    public void IncreaseScore()
+    {
+        score++;
+    }
+
+    // Check if the player's hand is empty
+    public bool IsHandEmpty()
+    {
+        return hand.Count == 0;
+    }
+}
