80e33c0798fdfd54697b352ae35c16e8da751e98
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2023-05-23 15:43:49 +0100

    230523 15:42 RC
    Completed main camera merge with canvas. Distinguished aiPanel and playerPanel for the use of selectable objects (not only cards selectable in the playerPanel can be clicked)

--- Assets/Scripts/CardUI.cs
+++ Assets/Scripts/CardUI.cs
@@ -1,153 +1,112 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CardUI : MonoBehaviour
 {
-    public Text nameText;
-    public Text speedText;
-    public Text wingspanText;
-    public Text strengthText;
-    public Text sizeText;
-    public Text weightText;
-    public Image cardImage;
-    public Image frameImage;
-
-    private bool dealt = false;
-
+    private bool isHovering = false;
     private Vector3 hoverOffset;
-    private Vector3 startingPoint;
+    private Transform startingParent;
     private CardDealer cardDealer; // Reference to the CardDealer script
+    private int startingSortingOrder;
 
     private void Awake()
     {
         hoverOffset = Vector3.zero;
         cardDealer = FindObjectOfType<CardDealer>(); // Find the CardDealer script in the scene
+        startingSortingOrder = GetComponent<Renderer>().sortingOrder;
+        startingParent = transform.parent;
     }
 
-    public void SetCardData(Card card)
+    public void OnPointerEnter()
     {
-        nameText.text = card.cardName;
-        speedText.text = "Speed: " + card.speed;
-        wingspanText.text = "Wingspan: " + card.wingspan;
-        strengthText.text = "Strength: " + card.strength;
-        sizeText.text = "Size: " + card.size;
-        weightText.text = "Weight: " + card.weight;
-        cardImage.sprite = card.thisImage;
+        if (startingParent == cardDealer.playerPanel.transform && !isHovering)
+        {
+            // Store the current position as the hover offset
+            hoverOffset = Vector3.up * 10f;
+            transform.localPosition += hoverOffset;
+            isHovering = true;
+        }
+    }
 
-        // Set frame color or material based on card color
-        if (card.color == "Grey")
+    public void OnPointerExit()
+    {
+        if (startingParent == cardDealer.playerPanel.transform && isHovering)
         {
-            frameImage.color = new Color32(128, 128, 128, 255);
+            // Reset the position by subtracting the hover offset
+            transform.localPosition -= hoverOffset;
+            hoverOffset = Vector3.zero;
+            isHovering = false;
         }
-        else if (card.color == "Blue")
+    }
+
+    public void OnPointerClick()
+    {
+        Debug.Log("Card clicked!");
+
+        // Get the main camera
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null)
         {
-            frameImage.color = new Color32(135, 206, 250, 255);
+            Debug.LogWarning("Main camera not found!");
+            return;
         }
-        else if (card.color == "Red")
+
+        // Calculate the center position of the screen
+        Vector3 centerScreenPosition = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f);
+
+        // Convert the center position from screen space to world space
+        Vector3 centerWorldPosition = mainCamera.ScreenToWorldPoint(centerScreenPosition);
+        centerWorldPosition.z = 0f; // Set the z position to 0 to avoid any depth issues
+
+        // Find the playerPanel in the scene hierarchy
+        GameObject playerPanel = GameObject.Find("playerPanel");
+        if (playerPanel != null)
         {
-            frameImage.color = new Color32(220, 20, 60, 255);
+            // Move the card to the center position only if it belongs to the playerPanel
+            if (transform.IsChildOf(playerPanel.transform))
+            {
+                // Move the card to the center position
+                transform.position = centerWorldPosition;
+
+                // Enlarge the card by 50%
+                transform.localScale *= 1.5f;
+
+                // Adjust the sorting order to ensure the card is on top
+                Renderer renderer = GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.sortingOrder = GetHighestSortingOrder() + 1;
+                }
+            }
         }
-        else if (card.color == "Green")
+        else
         {
-            frameImage.color = new Color32(173, 255, 47, 255);
+            Debug.LogWarning("playerPanel not found!");
         }
-        else if (card.color == "Gold")
+    }
+
+
+
+    private int GetHighestSortingOrder()
+    {
+        int highestSortingOrder = int.MinValue;
+
+        // Find all the cards in the scene
+        CardUI[] cards = FindObjectsOfType<CardUI>();
+
+        foreach (CardUI card in cards)
         {
-            frameImage.color = new Color32(212, 175, 55, 255);
+            Renderer cardRenderer = card.GetComponent<Renderer>();
+            if (cardRenderer != null)
+            {
+                highestSortingOrder = Mathf.Max(highestSortingOrder, cardRenderer.sortingOrder);
+            }
         }
-        else if (card.color == "Rainbow")
-        {
-            Material rainbowMaterial = Resources.Load<Material>("Rainbow");
-            frameImage.material = rainbowMaterial;
-        }
+
+        return highestSortingOrder;
     }
 
     public void ScaleDown(float scale)
     {
         transform.localScale = new Vector3(scale, scale, 1f);
     }
-
-    public void OnPointerEnter()
-    {
-        // Store the current position as the hover offset
-        hoverOffset = Vector3.up * 30f;
-        transform.position += hoverOffset;
-    }
-
-    public void OnPointerExit()
-    {
-        // Reset the position by subtracting the hover offset
-        transform.position -= hoverOffset;
-        hoverOffset = Vector3.zero;
-    }
-
-    //public void OnPointerClick()
-    //{
-    //    // Check if the card is already in the player panel or AI panel
-    //    Transform playerPanelTransform = cardDealer.playerPanel.transform;
-    //    Transform aiPanelTransform = cardDealer.aiPanel.transform;
-    //    bool isInPlayerPanel = transform.IsChildOf(playerPanelTransform);
-    //    bool isInAiPanel = transform.IsChildOf(aiPanelTransform);
-
-    //    // Store the starting position
-    //    startingPoint = transform.position;
-
-    //    if (isInPlayerPanel || isInAiPanel)
-    //    {
-    //        // Move the card back to its original panel
-    //        transform.SetParent(isInPlayerPanel ? playerPanelTransform : aiPanelTransform);
-    //        transform.localScale = Vector3.one;
-    //    }
-    //    else
-    //    {
-    //        // Calculate the center position of the screen in world coordinates
-    //        Vector3 centerScreenPosition = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane));
-
-    //        // Add an offset to the vertical position
-    //        centerScreenPosition.y += 450f;
-    //        centerScreenPosition.x += 975;
-
-    //        // Move the card to the center of the screen
-    //        transform.position = centerScreenPosition;
-    //    }
-    //}
-
-    public void OnPointerClick()
-    {
-        Transform playerPanelTransform = cardDealer.playerPanel.transform;
-        Transform aiPanelTransform = cardDealer.aiPanel.transform;
-        bool isInPlayerPanel = transform.IsChildOf(playerPanelTransform);
-        bool isInAiPanel = transform.IsChildOf(aiPanelTransform);
-
-        
-
-
-        // Calculate the center position of the screen
-        Vector3 centerScreenPosition = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
-
-        centerScreenPosition.y += 450;
-        centerScreenPosition.x += 975;
-
-        if(dealt)
-        {
-            transform.position = startingPoint;
-            dealt = false;
-        }
-        else
-        {
-            // Store the starting position
-            startingPoint = transform.position;
-            transform.position = centerScreenPosition;
-            dealt = true;
-        }
-
-    }
-
-    public void OnPointerClickAgain()
-    {
-        
-    }
-
-
-
 }
